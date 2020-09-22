63d4d379bb5879d29d7c6e1e0a2af37d5f0bae76
sangam deuja <sangamad1@gmail.com>
2020-09-22 17:37:44 +0300

    Exercise 2.9 added

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,14 +2,19 @@ import React, { useState } from 'react'
 import Person from './components/Person'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas',number:123456 }
-  ]) 
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
 
   
   const [ newName, setNewName ] = useState('')
 
   const [ newPhone, setNewPhone ] = useState('')
+
+  const [ searchName, setsearchName ] = useState('')
 
   const addName=(Event)=>{
     Event.preventDefault()
@@ -30,9 +35,27 @@ const App = () => {
     setNewPhone(Event.target.value)
   }
 
+
+  const handleSearch=(Event)=>{
+    Event.preventDefault()
+    setsearchName(Event.target.value)
+  
+  }
+
+  const result=persons.map(person=>person.name).map(word=>word.includes(searchName))
+  const cleaned=persons.filter((curr, i) => result[i] && curr)
+  
+
   return (
+  
     <div>
       <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          filter shown with: <input value={searchName} onChange={handleSearch}/>
+        </div>
+      </form>
+      <h2>add a new</h2>
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
@@ -48,7 +71,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <>
-        {persons.map((person, i) => 
+        {cleaned.map((person) => 
           <Person key={person.name} person={person} />
         )}
       </>
--- part2/phonebook/src/components/Person.js
+++ part2/phonebook/src/components/Person.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Person=({person})=>{
-    console.log(person)
+    
     return (
     <p>{person.name}  {person.number}</p>
     )
